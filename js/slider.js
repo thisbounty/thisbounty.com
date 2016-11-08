c9e90ea76f82b516ecc7b2c9f9281cff731f93bb
Steve <urgent.programmer@gmail.com>
2016-11-08 08:34:20 -0500

    Slider -- Fix scrollbar
    Needed css fixes for scrollbar, not hidden with original flex box, needed
    to be hidden, shown

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,27 @@
 var i=0;
+
+slide_scroll_hide();
+
 $("body").on('click', '#nextSlide', function() {
   i++;
   if(i == $('.slide').length) {
-    $('.slide.active').removeClass('active out');
+    $('.slide.active').removeClass('active').removeClass('out');
     $($('.slide')[0]).addClass('active');
+    slide_scroll_hide();
     i=0;
   }
   else{
     $('.slide.active').addClass('out').next('.slide').addClass('active');
+    slide_scroll_hide();
   }
   return false;
 });
+
+function slide_scroll_hide() {
+    //needed for scrollable div
+    $('.visible').removeClass('visible');
+    $('.invisible').removeClass('invisible');
+    $('.slide').addClass('invisible');
+    $('.slide.active').addClass('visible').removeClass('invisible');
+
+}
