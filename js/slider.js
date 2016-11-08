8b0a0f5f3d99159c50438e1c0b6609cf7fade479
Steve <urgent.programmer@gmail.com>
2016-11-08 17:59:31 -0500

    Slider -- More Data
    Added more projects to slider

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,19 @@
 var i=0;
 
-slide_scroll_hide();
+//slide_scroll_hide();
 
 $("body").on('click', '#nextSlide', function() {
   i++;
   if(i == $('.slide').length) {
-    $('.slide.active').removeClass('active').removeClass('out');
     $($('.slide')[0]).addClass('active');
-    slide_scroll_hide();
     i=0;
   }
   else{
-    $('.slide.active').addClass('out').next('.slide').addClass('active');
-    slide_scroll_hide();
+    $('.slide.active').addClass('out');
+    $('.slide.active').next('.slide').addClass('active');
+    $('.slide.active.out').removeClass('active').removeClass('out');
   }
+  //slide_scroll_hide();
   return false;
 });
 
@@ -23,5 +23,4 @@ function slide_scroll_hide() {
     $('.invisible').removeClass('invisible');
     $('.slide').addClass('invisible');
     $('.slide.active').addClass('visible').removeClass('invisible');
-
 }
